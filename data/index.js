c26801dfb12c3b27e42acdd5255f5b89186e9983
saty06 <saty06072000@gmail.com>
2024-07-12 02:05:31 -0700

    [satya]=>  add more example code

---
+++ data/index.js
@@ -0,0 +1,10 @@
+const fruits = {Bananas:300, Oranges:200, Apples:500};
+let res = Object.entries(fruits);
+console.log(res)
+
+// let text = "";
+// for (let [fruit, value] of Object.entries(fruits)) {
+// console.log(fruit,"=>", value)
+// }
+let x = 9.656;
+console.log(x.valueOf());
