7462c5ba28405185562a1588e51ff54cb9066160
saty06 <saty06072000@gmail.com>
2024-07-15 15:48:23 +0530

    [satya]=> cast file add

--- data/index.js
+++ data/index.js
@@ -1,10 +1,23 @@
-const fruits = {Bananas:300, Oranges:200, Apples:500};
-let res = Object.entries(fruits);
-console.log(res)
-
-// let text = "";
-// for (let [fruit, value] of Object.entries(fruits)) {
-// console.log(fruit,"=>", value)
+// //  01 
+// let  num = 3;
+// let result = 0;
+// while(num>=0){
+//     let data = Math.pow(num,2);
+//     result = result+data
+//     num = num-1
 // }
-let x = 9.656;
-console.log(x.valueOf());
+// console.log(result)
+// // 02 
+// function om(arg){
+//     let res = 0;
+//     for(let i =0; i<arg.length; i++){
+//         res = res+arg[i]
+//     }
+//     return res+" om"
+// }
+// let number = [1,2,3,4,55,5]
+// let num2 = om(number)
+// console.log(num2)
+let hexa = 0xff2
+let data2 =hexa.toString()
+console.log(  hexa )
