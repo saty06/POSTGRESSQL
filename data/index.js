8e552709e56f2c7cc7390f8e8eff95d397ef6943
saty06 <saty06072000@gmail.com>
2024-07-16 16:28:31 +0530

    [satya]add:add a txt file which is store how many queary i used

--- data/index.js
+++ data/index.js
@@ -18,6 +18,45 @@
 // let number = [1,2,3,4,55,5]
 // let num2 = om(number)
 // console.log(num2)
-let hexa = 0xff2
-let data2 =hexa.toString()
-console.log(  hexa )
+// let hexa = 0xff2
+// let data2 =hexa.toString()
+// console.log(  hexa )
+// Write a JavaScript program to rotate the string 'w3resource' in the right direction. This
+//  is done by periodically removing one letter from the string end and attaching it to the front.
+let str = 'w3resource'
+let str2 = str.split('')// [w,3,r,e,s,o,u,r,c,e]
+for(let i =1; i<=3; i++){
+    let first = str2[0]
+    let last = str2[str2.length-1]
+    str2.shift()
+    str2.push(first)
+    first = null
+
+    
+
+}
+console.log(str2.join(''))
+// Write a JavaScript program to 
+// determine whether a given year is a leap year
+//  in the Gregorian calendar.
+function leep( leep){
+    let count = 0
+    if(leep<=0){
+        return " invalid data"
+    }
+    // leeep year 
+    if(leep%100==0){
+        if(leep%4){
+            count++
+            
+        }
+    }
+    if(leep%4==0){
+        count++;
+    }
+    return count
+
+}
+let leepData = -1999
+let result = leep(leepData);
+console.log(result)
